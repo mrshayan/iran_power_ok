13509dd71509d27a0e11d417a4a119e2c458d8f6
shayanchabok007 <shayanchabok5@gmail.com>
2016-03-15 14:39:45 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -251,7 +251,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {181742153,95822747},--Sudo users
+    sudo_users = {181742153},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
