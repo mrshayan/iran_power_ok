99ddeeee07c035c27dfd038f48fc919096321c7a
shayanchabok007 <shayanchabok5@gmail.com>
2016-03-16 15:13:22 +0430

    Update sendsticker.lua

--- plugins/sendsticker.lua
+++ plugins/sendsticker.lua
@@ -1,14 +1,13 @@
-do
 
 function run(msg, matches)
-  if matches[1]:lower() == 'تبدیل به استیکر' then -- Put everything you like :)
+  if matches[1]:lower() == 'sbss' then -- Put everything you like :)
     send_document(get_receiver(msg), "./files/sticker.webp", ok_cb, false)
     return 'test'
   end
 end
 return {
   patterns = {
-    "^تبدیل به استیکر$"
+    "^تبدیل$"
   }, 
   run = run 
 }
@@ -19,4 +18,3 @@ end
 --change file name to sticker.webp
 --enjoy
 --special thank to mehr pouya and gamer team
-
